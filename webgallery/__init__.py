ee24678f590665955608cdd2549a22247f5ceb85
Vincent Girès <mail@vincentgires.com>
2020-10-11 15:31:21 +0200

    Clean not used functions

--- webgallery/__init__.py
+++ webgallery/__init__.py
@@ -29,98 +29,6 @@ def get_media_folderpath():
     return folderpath
 
 
-def get_collections_folderpath():
-    folderpath = os.environ.get('GALLERY_PATH', app.static_folder)
-    folderpath = os.path.join(folderpath, 'media', 'collections')
-    return folderpath
-
-
-def get_collection_folders():
-    folderpath = get_collections_folderpath()
-    galleries = [
-        i for i in os.listdir(folderpath)
-        if os.path.isdir(os.path.join(folderpath, i))]
-    for i in galleries:
-        config_file = os.path.join(folderpath, i, COLLECTION_CONFIG_FILE)
-        is_config = os.path.exists(config_file)
-        if is_config:
-            with open(config_file) as f:
-                config = json.load(f)
-                if config.get('private'):
-                    galleries.remove(i)
-    return galleries
-
-
-def get_images(foldername, imagetype, fullpath=False):
-    folderpath = os.path.join(
-        get_collections_folderpath(), foldername, imagetype)
-    if os.path.isdir(folderpath):
-        images = [
-            i for i in sorted(os.listdir(folderpath))
-            if os.path.splitext(i)[1].lower() in SUPPORTED_IMAGE_EXT]
-        if fullpath:
-            return [os.path.join(folderpath, i) for i in images]
-        return images
-
-
-def get_videos(foldername):
-    folderpath = os.path.join(
-        get_collections_folderpath(), foldername, 'videos')
-    if os.path.isdir(folderpath):
-        videos = [
-            i for i in sorted(os.listdir(folderpath))
-            if os.path.splitext(i)[1].lower() in SUPPORTED_VIDEO_EXT]
-        return videos
-
-
-def generate_media_thumbail(filepath):
-    folderpath, filename = os.path.split(filepath)
-    outputpath = os.path.normpath(os.path.join(
-        folderpath, os.pardir, THUMBNAIL_FOLDERNAME))
-    if not os.path.exists(outputpath):
-        os.makedirs(outputpath)
-    outputpath = os.path.join(outputpath, filename)
-    command = [
-        'magick', 'convert',
-        filepath,
-        '-colorspace', 'rgb',
-        '-resize', '300x300',
-        '-colorspace', 'srgb',
-        outputpath]
-    subprocess.call(command)
-    return outputpath
-
-
-def generate_all_thumbails(foldername):
-    images = get_images(
-        foldername=foldername, imagetype=HIGHRES_FOLDERNAME, fullpath=True)
-    for filepath in images:
-        logging.info('thumbnail: {}'.format(filepath))
-        generate_media_thumbail(filepath)
-
-
-def generate_gallery_thumbail(foldername):
-    inputs = get_images(foldername, THUMBNAIL_FOLDERNAME, fullpath=True)
-    # exclude gif to avoid each images to be in the gallery thumbnail
-    inputs = [i for i in inputs if os.path.splitext(i)[1] != '.gif']
-    output = os.path.join(
-        get_collections_folderpath(),
-        foldername,
-        COLLECTION_IMG_FILENAME)
-    img_width = GALLERY_IMG_WIDTH / len(inputs)
-    command = ['magick', 'montage']
-    command.extend(inputs)
-    command.extend([
-        '-colorspace', 'rgb',
-        '-resize', 'x{h}'.format(h=GALLERY_IMG_HEIGHT),
-        '-crop', '{w}x{h}+0+0'.format(w=img_width, h=GALLERY_IMG_HEIGHT),
-        '-mode', 'concatenate',
-        '-tile', 'x1',
-        '-colorspace', 'srgb',
-        output])
-    subprocess.call(command)
-
-
 def get_json_files(category):
     path = os.path.join(get_media_folderpath(), category)
     return [i for i in sorted(os.listdir(path)) if i.endswith('.json')]
@@ -214,21 +122,22 @@ def show_collection(name):
 def upload_file():
     if request.method == 'POST':
         files = request.files.getlist('file')
-        collection_name = request.form['collection_name']
-        if not collection_name:
-            return redirect(request.url)
-        folderpath = os.path.join(
-            get_collections_folderpath(), collection_name, HIGHRES_FOLDERNAME)
-        if not os.path.exists(folderpath):
-            os.makedirs(folderpath)
-        for file in files:
-            if not file.filename:
-                flash('No selected file')
-                return redirect(request.url)
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(folderpath, filename)
-            file.save(filepath)
-            generate_media_thumbail(filepath)
+        # TODO
+        # collection_name = request.form['collection_name']
+        # if not collection_name:
+        #     return redirect(request.url)
+        # folderpath = os.path.join(
+        #     get_collections_folderpath(), collection_name, HIGHRES_FOLDERNAME)
+        # if not os.path.exists(folderpath):
+        #     os.makedirs(folderpath)
+        # for file in files:
+        #     if not file.filename:
+        #         flash('No selected file')
+        #         return redirect(request.url)
+        #     filename = secure_filename(file.filename)
+        #     filepath = os.path.join(folderpath, filename)
+        #     file.save(filepath)
+        #     # TODO: generate media thumbnail
     return render_template('add.html')
 
 
